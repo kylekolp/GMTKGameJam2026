aceb596f6aee70a1f9a5c60a7d7706847e5c1e93
Kyle Kolpack <kylekolpack@gmail.com>
2026-07-12 12:22:23 -0400

    Moved a couple files around to appropriate places. Added a signal bus
    Some of the temp scripts I made weren't in their correct directories so I moved them. I also added a core SignalBus class which we can use for creating any signals that need to be global.

---
+++ src/core/systems/Signals/SignalBus.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# Use this class to emit any custom signal that we wish to access globally. It is auto loaded so it can be treated as a singleton (Accessable from anywhere)
+# Note: Not all signals have to be global. Only use for signals that are required to be received outside the emitting node 
+# 		(For example children can connect to a parents node via the signals tab without being global)
+#
+# Example
+# signal testSignal(source : Area2D)
+#
+# Then in any class that wishes to receive the signal just call:
+# SignalBus.testSignal.connect(testSignalMethod) <--- this binds the signal to a function "testSignalMethod" which will be called whever the signal is emitted.
+#
+# To Emit the signal just call
+# SignalBus.testSignal.emit(someArea2D)
