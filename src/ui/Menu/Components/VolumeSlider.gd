a45f7a01070a02a8850d6be08732aecfc4602225
Kyle Kolpack <kylekolpack@gmail.com>
2026-07-13 21:04:13 -0400

    Hooked up options menu volume sliders
    Set up audio busses and set up volume sliders for the options and future pause menu

---
+++ src/ui/Menu/Components/VolumeSlider.gd
@@ -0,0 +1,15 @@
+extends HSlider
+
+#The name of the audio bus we are adjusting
+@export var busName : String
+
+var busIndex : int
+
+func _ready() -> void:
+	busIndex = AudioServer.get_bus_index(busName)
+	value_changed.connect(_on_value_changed)
+	
+	value = db_to_linear(AudioServer.get_bus_volume_db(busIndex))
+	
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(busIndex,linear_to_db(value))
