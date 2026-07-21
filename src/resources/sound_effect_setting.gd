c77b2f6fb7740acc120fc702ead82e3a4683c8a5
etonnote <75054886+etonnote@users.noreply.github.com>
2026-07-21 10:36:50 -0400

    AudioManager setup

---
+++ src/resources/sound_effect_setting.gd
@@ -0,0 +1,26 @@
+class_name SoundEffect
+extends Resource
+
+# Different types of sound effects available to be played, instaniate with AudioManager.create_2d_audio or AudioManager.create_audio
+enum SOUND_EFFECT_TYPE{ 
+	PLACEHOLDER,
+}
+
+@export_range(0, 10) var limit: int = 5 ## Max number of SoundEffect allowed to be played
+@export var type: SOUND_EFFECT_TYPE ## Unique sfx in the enum SOUND_EFFECT_TYPE
+@export var sound_effect: AudioStream ## audio file to be played
+@export_range (-40, 20) var volume: float = 0 ## Volume of sfx
+@export_range(0.0, 4.0, 0.1) var pitch_scale: float = 1.0 # Pitch of sfx
+@export_range(0.0, 1.0, 0.01) var pitch_randomness: float = 0.0 # +/- range of pitch randomization
+
+var audio_count: int = 0 # Instances of sound_effect currently playing
+
+func change_audio_count(amount: int):
+	audio_count = max(0, audio_count + amount)
+
+## Check if there is room for another instance
+func has_open_limit() -> bool:
+	return audio_count < limit
+
+func on_audio_finished():
+	change_audio_count(-1)
--- src/ui/Menu/Menu_Main.gd
+++ src/ui/Menu/Menu_Main.gd
@@ -3,10 +3,13 @@ extends BaseMenu
 func _on_start_game_button_pressed() -> void:
 	#To Do: Create Start Level
 	#SignalBus.emit_signal("LoadLevel","Level1UID")
+	AudioManager.create_audio(SoundEffect.SOUND_EFFECT_TYPE.PLACEHOLDER)
 	return
 
 func _on_options_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffect.SOUND_EFFECT_TYPE.PLACEHOLDER)
 	SignalBus.LoadMenu.emit(UIDCatalog.Menu_Options)
 
 func _on_quit_game_button_pressed() -> void:
+	AudioManager.create_audio(SoundEffect.SOUND_EFFECT_TYPE.PLACEHOLDER)
 	SignalBus.TryQuit.emit()
