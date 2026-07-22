b49f976465191f2d8c459a63b4df54b5e7b6f73a
Kyle Kolpack <kylekolpack@gmail.com>
2026-07-22 18:23:32 -0400

    Created temp script for rope movement

--- src/gameplay/entities/Entity_RopeSegment.gd
+++ src/gameplay/entities/Entity_RopeSegment.gd
@@ -3,6 +3,13 @@ extends RigidBody2D
 
 @export var player : Player
 
+var _speed : float = 1000
+
 func _physics_process(delta: float) -> void:
+	
+	var directionVector = global_position.direction_to(player.global_position);
+	
+	linear_velocity = directionVector * _speed
+	
 	return
 	#var playerPosition =
