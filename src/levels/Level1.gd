7fa9f8ced9d110e049e4bafa27a1a78a956a1dba
Sev N <31839316+ajtran303@users.noreply.github.com>
2026-07-23 11:05:51 -0400

    rockets spawn in area over time

--- src/levels/Level1.gd
+++ src/levels/Level1.gd
@@ -1,10 +1,28 @@
 extends Level
 
+@export var rocket_spawn_interval: float = 2.0
+
+@onready var rocket_spawn_timer: Timer = $RocketSpawnTimer
+@onready var rocket_spawn_area_top_left: Marker2D = $SpawnAreaTopLeft
+@onready var rocket_spawn_area_bottom_right: Marker2D = $SpawnAreaBottomRight
+
+var rocket_spawn_area: Rect2
+
 func _ready() -> void:
 	SignalBus.LoadEntity.emit(UIDCatalog.Entity_Player, Vector2(100,100), self)
-	SignalBus.LoadEntity.emit(UIDCatalog.Entity_Rocket, Vector2(600,600), self)
-	SignalBus.LoadEntity.emit(UIDCatalog.Entity_Rocket, Vector2(800,200), self)
-	SignalBus.LoadEntity.emit(UIDCatalog.Entity_Rocket, Vector2(400,250), self)
-	SignalBus.LoadEntity.emit(UIDCatalog.Entity_Rocket, Vector2(300,400), self)
 	SignalBus.LoadEntity.emit(UIDCatalog.Entity_Fire, Vector2(100,600), self)
 	SignalBus.LoadEntity.emit(UIDCatalog.Entity_Fire, Vector2(1000,600), self)
+	
+	rocket_spawn_area = Rect2(
+		rocket_spawn_area_top_left.position,
+		rocket_spawn_area_bottom_right.position - rocket_spawn_area_top_left.position
+	)
+	
+	rocket_spawn_timer.start()
+
+func _on_rocket_spawn_timer_timeout() -> void:
+	var rocket_spawn_position := Vector2(
+		randf_range(rocket_spawn_area.position.x, rocket_spawn_area.end.x),
+		randf_range(rocket_spawn_area.position.y, rocket_spawn_area.end.y)
+	)
+	SignalBus.LoadEntity.emit(UIDCatalog.Entity_Rocket, rocket_spawn_position, self)
