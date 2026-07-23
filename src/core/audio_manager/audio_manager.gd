abeb16640db75842cc912d0873511cbb40b4859c
etonnote <75054886+etonnote@users.noreply.github.com>
2026-07-23 16:48:12 -0400

    Fire and Rocket Spawn SFX

--- src/core/audio_manager/audio_manager.gd
+++ src/core/audio_manager/audio_manager.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 		sound_effect_dict[sound_effect.type] = sound_effect
 
 
-func create_2d_audio(location: Vector2, type: SoundEffect.SOUND_EFFECT_TYPE):
+func create_2d_audio(location: Vector2, distance: int, type: SoundEffect.SOUND_EFFECT_TYPE):
 	if sound_effect_dict.has(type):
 		var sound_effect: SoundEffect = sound_effect_dict[type]
 		if sound_effect.has_open_limit():
@@ -19,11 +19,13 @@ func create_2d_audio(location: Vector2, type: SoundEffect.SOUND_EFFECT_TYPE):
 			new_2D_audio.position = location
 			new_2D_audio.stream = sound_effect.sound_effect
 			new_2D_audio.volume_db = sound_effect.volume
+			new_2D_audio.max_distance = distance
 			new_2D_audio.pitch_scale = sound_effect.pitch_scale
 			new_2D_audio.pitch_scale += randf_range(-sound_effect.pitch_randomness, sound_effect.pitch_randomness)
 			new_2D_audio.finished.connect(sound_effect.on_audio_finished)
 			new_2D_audio.finished.connect(new_2D_audio.queue_free)
 			new_2D_audio.play()
+			return new_2D_audio
 	else:
 		push_error("Audio Manager failed to find the setting for this type ", type)
 
@@ -44,3 +46,12 @@ func create_audio(type: SoundEffect.SOUND_EFFECT_TYPE):
 	else:
 		push_error("Audio Manager failed to find the setting for this type ", type)
 		
+
+func update_position(sfx: AudioStreamPlayer2D, location: Vector2):
+	sfx.position = location
+	
+
+func fade_out(sfx:AudioStreamPlayer2D, duration: float = 1.0):
+	var tween = create_tween()
+	tween.tween_property(sfx, "volume_db", -80, duration)
+	
--- src/resources/sound_effect_setting.gd
+++ src/resources/sound_effect_setting.gd
@@ -4,9 +4,12 @@ extends Resource
 # Different types of sound effects available to be played, instaniate with AudioManager.create_2d_audio or AudioManager.create_audio
 enum SOUND_EFFECT_TYPE{ 
 	PLACEHOLDER,
+	FIRE,
+	ROCKET_SPAWN,
+	PLAYER_HIT,
 }
 
-@export_range(0, 10) var limit: int = 5 ## Max number of SoundEffect allowed to be played
+@export_range(0, 100) var limit: int = 5 ## Max number of SoundEffect allowed to be played
 @export var type: SOUND_EFFECT_TYPE ## Unique sfx in the enum SOUND_EFFECT_TYPE
 @export var sound_effect: AudioStream ## audio file to be played
 @export_range (-40, 20) var volume: float = 0 ## Volume of sfx
