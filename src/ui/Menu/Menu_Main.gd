95af86f7082de39102efd4290bdb5600fc4ea028
Kyle Kolpack <kylekolpack@gmail.com>
2026-07-25 19:32:07 -0400

    Disabled boom and applause on additional game loads

--- src/ui/Menu/Menu_Main.gd
+++ src/ui/Menu/Menu_Main.gd
@@ -30,14 +30,25 @@ func _on_quit_button_pressed() -> void:
 
 func RunStartAnimation(isFirstRun : bool):
 	if !isFirstRun:
+		var anim: Animation = animationPlayer.get_animation("MoveBackground")
+		
+		var animTrackFirework : int = anim.track_find_key(29,11.35)
+		var animTrackAudience : int = anim.track_find_key(32,11.2)
+		
+		if animTrackFirework != -1:
+			anim.track_remove_key(29, animTrackFirework)
+			
+		if animTrackAudience != -1:
+			anim.track_remove_key(32, animTrackAudience)
+		
 		animationPlayer.play("MoveBackground")
 		animationPlayer.seek(10.3,true)
-		await get_tree().create_timer(12).timeout
+		await get_tree().create_timer(3.1).timeout
 		animationPlayer.play("Idle")
 	else:
 		animationPlayer.play("MoveBackground")
 		animationPlayer.seek(5,true)
-		await get_tree().create_timer(7).timeout
+		await get_tree().create_timer(6.7).timeout
 		animationPlayer.play("Idle")
 		SignalBus.MainMenuIntroRan.emit()
 		
